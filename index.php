87f7ae70a23d6e937e77e6e29b79e49472f7fc4f
Alexandre151202 <100602977+Alexandre151202@users.noreply.github.com>
2022-05-04 11:18:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'Car.php';
+require once 'Speedometer.php';
 
 $twingo = new Car('red', 4, 'low');
 
@@ -14,3 +15,6 @@ try {
 
 echo $twingo->forward() . '<br>';
 echo $twingo->brake() . '<br>';
+
+echo Speedometer::convertKmToMiles(10) . PHP_EOL;
+echo Speedometer::convertMilesToKm(6.21) . PHP_EOL;
